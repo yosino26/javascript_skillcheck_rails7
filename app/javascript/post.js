e226efed546fbf87550d69d2cb3d77226d25d2c0
yosino26 <fn.yoshino57@gmail.com>
2025-06-15 05:56:36 +0900

    レスポンスを表示するように編集

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -7,6 +7,15 @@ function post (){
     XHR.responseType = "json";
     XHR.send(formData);
     XHR.onload = () => {
+      const item = XHR.response.article;
+      const contentsArea = document.getElementById("contents_area");
+      const articleText = document.getElementById("article_text");
+      const HTML = `
+        <div class="article">
+          ${ item.text }
+        </div>`;
+      contentsArea.insertAdjacentHTML("afterbegin", HTML);
+      articleText.value = "";
     };
     e.preventDefault();
   });
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'articles#index'
-  resources :articles
+  # resources :articles
+  resources :articles, except: :create
 end

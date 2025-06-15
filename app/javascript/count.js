0a482d10513c5aaa0c05339efec7ef24bdd7846d
yosino26 <fn.yoshino57@gmail.com>
2025-06-15 13:00:19 +0900

    文字数をカウントのイベントを確認

--- app/javascript/count.js
+++ app/javascript/count.js
@@ -1,5 +1,8 @@
 function count (){
-  
+  const articleText  = document.getElementById("article_text");
+  articleText.addEventListener("keyup", () => {
+    console.log(articleText.value.length);
+  });
 };
 
 window.addEventListener('turbo:load', count);
